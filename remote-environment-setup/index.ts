39c73c3ab3d695757ca2bb401226db1814014ca5
Frank Dekervel <kervel@gmail.com>
2021-12-03 13:39:21 +0100

    fix

--- remote-environment-setup/index.ts
+++ remote-environment-setup/index.ts
@@ -1,6 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as hcloud from "@pulumi/hcloud";
 import * as fs from "fs";
+import * as path from "path";
+import * as process from "process";
 import * as cloudinit from "@pulumi/cloudinit";
 
 class Machine {
@@ -20,8 +22,13 @@ const machines = [
     new Machine("machine2", "holapola", "sdoiijoijs"),
 ];
 
+let homeDir = "";
+if (process.env.HOME) {
+    homeDir = process.env.HOME;
+}
+
 const sshKey = new hcloud.SshKey("creatorkey", {
-    publicKey: fs.readFileSync("/home/kervel/.ssh/id_rsa.pub").toString()
+    publicKey: fs.readFileSync(path.join(homeDir,  ".ssh/id_rsa.pub")).toString()
 })
 
 
@@ -56,7 +63,7 @@ machines.forEach(m => {
     curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64 && \
     sudo install skaffold /usr/local/bin/
 
-    curl -Lo k9s.tgz https://github.com/derailed/k9s/releases/download/v0.25.7/k9s_v0.25.7_Linux_x86_64.tar.gz && \
+    curl -Lo k9s.tgz https://github.com/derailed/k9s/releases/download/v0.25.7/k9s_Linux_x86_64.tar.gz && \
     tar -xf k9s.tgz  && sudo install k9s /usr/local/bin/
 
     curl -Lo kubectx https://github.com/ahmetb/kubectx/releases/download/v0.9.3/kubectx && \
@@ -81,7 +88,7 @@ machines.forEach(m => {
     
     const vm = new hcloud.Server(m.name, {
         image: "ubuntu-20.04",
-        serverType: "cx31-ceph",
+        serverType: "cx41",
         location: "fsn1",
         sshKeys:  [sshKey.id],
         userData: resourceConf.rendered
